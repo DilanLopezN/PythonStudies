6d5876c9e1b661503619f61ae8d36a712ad4ccf2
Dilan lopez <dilanlopez009@gmail.com>
2023-08-24 23:12:18 -0300

    type base

---
+++ Fundamentals/fundamentals.py
@@ -0,0 +1,11 @@
+# STRING
+print("hello python");
+# INT E FLOAT
+print(1 + 1.1);
+# BOOLEAN
+print(False, True);
+# COERÇÃO
+print(int('1') + 1);
+# VAR
+my_full_name = "Dilan lopez do Nascimento";
+print(my_full_name);
